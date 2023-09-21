7ff9a9ed865cc23cf905f5963b5df39d8cd6d07f
Madderchod <145271953+Madderchod@users.noreply.github.com>
2023-09-21 12:22:23 +0600

    Add files via upload

---
+++ neha.py
@@ -0,0 +1,73 @@
+import os
+os.system("clear")
+
+logo = ("""
+\033[31;1m                                                                                                              
+                                                                                           
+NNNNNNNN        NNNNNNNN                   hhhhhhh                                         
+N:::::::N       N::::::N                   h:::::h                                         
+N::::::::N      N::::::N                   h:::::h                                         
+N:::::::::N     N::::::N                   h:::::h                                         
+N::::::::::N    N::::::N    eeeeeeeeeeee    h::::h hhhhh         aaaaaaaaaaaaa             
+N:::::::::::N   N::::::N  ee::::::::::::ee  h::::hh:::::hhh      a::::::::::::a            
+N:::::::N::::N  N::::::N e::::::eeeee:::::eeh::::::::::::::hh    aaaaaaaaa:::::a           
+N::::::N N::::N N::::::Ne::::::e     e:::::eh:::::::hhh::::::h            a::::a           
+N::::::N  N::::N:::::::Ne:::::::eeeee::::::eh::::::h   h::::::h    aaaaaaa:::::a           
+N::::::N   N:::::::::::Ne:::::::::::::::::e h:::::h     h:::::h  aa::::::::::::a           
+N::::::N    N::::::::::Ne::::::eeeeeeeeeee  h:::::h     h:::::h a::::aaaa::::::a           
+N::::::N     N:::::::::Ne:::::::e           h:::::h     h:::::ha::::a    a:::::a           
+N::::::N      N::::::::Ne::::::::e          h:::::h     h:::::ha::::a    a:::::a           
+N::::::N       N:::::::N e::::::::eeeeeeee  h:::::h     h:::::ha:::::aaaa::::::a           
+N::::::N        N::::::N  ee:::::::::::::e  h:::::h     h:::::h a::::::::::aa:::a          
+NNNNNNNN         NNNNNNN    eeeeeeeeeeeeee  hhhhhhh     hhhhhhh  aaaaaaaaaa  aaaa          
+                                                                                           
+                                                                                           
+                                                                                           
+                                                                                           
+                                                                                           
+                                                                                           
+                                                                                           
+\033[32;1m           ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+\033[32;1m           ┃\033[34;1m TOOLS NAME => Neha            ┃
+\033[32;1m           ┃\033[32;1m OWNER NAME => Neha              ┃
+\033[32;1m           ┃\033[33;1m CLONE SYS  => DATA , WIFI              ┃
+\033[32;1m           ┃\033[32;1m WhatsApp nember:01981435815          ┃
+\033[32;1m           ┃\033[35;1m telegram account : @rkhacke1256raz       ┃
+\033[32;1m           ┃\033[31;1m      PLEASE DON'T UES A RONG WORK      ┃
+\033[32;1m           ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛""")
+
+print(logo)
+print("[1] Neha ")
+print("[2] Neha )
+print("[3] Neha ")
+print("[4] Neha ")
+
+x = input('CHOICE ')
+if x in ['1']:
+	os.system("clear")
+	print(logo)
+	print("PLZ FUCK EMRAN & EMRAN SISTER & EMRAN MATHER 💋💋 FUCK EMRAN SCRIPT OPEN SOURCE  https://github.com/HE4ON-AFRIDI/Fuck_Emran")
+	
+x = input('EMRAN FUCK COMPLITE ✅ ')
+	
+if x in ['2']:
+	os.system("clear")
+	print(logo)
+	print("PLZ FUCK EMRAN & EMRAN SISTER & EMRAN MATHER 💋💋 FUCK EMRAN SCRIPT OPEN SOURCE  https://github.com/HE4ON-AFRIDI/Fuck_Emran")
+	
+x = input('Neha  COMPLITE ✅ ')
+	
+if x in ['3']:
+	os.system("clear")
+	print(logo)
+	print("PLZ FUCK EMRAN & EMRAN SISTER & EMRAN MATHER 💋💋 FUCK EMRAN SCRIPT OPEN SOURCE  https://github.com/HE4ON-AFRIDI/Fuck_Emran")
+	
+x = input('EMRAN FUCK COMPLITE ✅ ')
+	
+if x in ['4']:
+	os.system("clear")
+	print(logo)
+	print("PLZ FUCK NAHID & NAHID SISTER & NAHID MATHER 💋💋 FUCK NAHID SCRIPT OPEN SOURCE  https://github.com/PRINCE-ARAFAT/Open-Source")
+	
+x = input('Neha COMPLITE ✅ ')
+		
